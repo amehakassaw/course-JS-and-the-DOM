75bf899a41db36fc05605a6c06f0878f6417836a
amehakassaw <amehakassaw6@gmail.com>
2024-10-21 21:50:04 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const mainHeading = document.querySelector('h1');
+const otherHeading = document.querySelector('h4');
+const excitedText = document.createElement('span');
+
+excitedText.textContent = '!!!';
+mainHeading.appendChild(excitedText);
+otherHeading.appendChild(excitedText);
